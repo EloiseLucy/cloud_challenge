c845ade600ac8329f58db608f18e92cbfe03a4fe
EloiseLucy <124316301+EloiseLucy@users.noreply.github.com>
2023-03-04 21:17:50 +0100

    commit

--- task_3_optional_function/hello_function.py
+++ task_3_optional_function/hello_function.py
@@ -3,11 +3,9 @@ def hello_function():
     print("Hello from the other siiiiiiiiiide!")
 
 
-
 def my_function_with_args(name, surname):
 
     print("Hello, it's me, %s %s"%(Lucyna, Zieciowska))
-
 
 
 def sum_two_numbers(a, b):
@@ -15,13 +13,10 @@ def sum_two_numbers(a, b):
     return a + b
 
 
-
 hello_function()
 
 
-
 my_function_with_args("Lucyna", "Zieciowska")
-
 
 
 x = sum_two_numbers(1,2)
