907347d6a9c5fd9b462719238c462de46a3d7b91
EloiseLucy <124316301+EloiseLucy@users.noreply.github.com>
2023-03-22 00:14:54 +0100

    c

--- task_3_optional_function/hello_function.py
+++ task_3_optional_function/hello_function.py
@@ -1,24 +1,12 @@
 def hello_function():
 
     print("Hello from the other siiiiiiiiiide!")
-
-
-def my_function_with_args(name, surname):
-
-    print("Hello, it's me, %s %s"%(name, surname))
-
-
-def sum_two_numbers(a, b):
-
-    return a + b
-
-
-hello_function()
-
-
-my_function_with_args("Lucyna", "Zieciowska")
-
-
-x = sum_two_numbers(1,2)
-
-print(x)
+Welcome to Cloud Shell! Type "help" to get started.
+Your Cloud Platform project in this session is set to august-craft-376622.
+Use “gcloud config set project [PROJECT_ID]” to change to a different project.
+lucyna_ciaston@cloudshell:~ (august-craft-376622)$ vim hello_function2.py
+lucyna_ciaston@cloudshell:~ (august-craft-376622)$ python hello_function2.py
+Hello from the other siiiiiiiiiide!
+Hello, it's me, Lucynna Zieciowska
+3
+lucyna_ciaston@cloudshell:~ (august-craft-376622)$
